abf1696adca78ec090c5627b850f5577cc97f954
Setiadi Prawiryo Moeljadi <piyoo86@gmail.com>
2021-05-23 08:45:58 +0700

    lazy load

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,28 +2,21 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { StockFormComponent } from './stock-form/stock-form.component';
 import { StockItemComponent } from './stock-item/stock-item.component';
 import { StockListComponent } from './stock-list/stock-list.component';
-import { PokemonDetailComponent } from './pokemon-detail/pokemon-detail.component';
-import { PokemonItemComponent } from './pokemon-item/pokemon-item.component';
-import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
 
 const routes: Routes = [
   { path: '', component: StockListComponent },
-  { path: 'pokemon', component: PokemonListComponent },
-  { path: 'pokemon/:id', component: PokemonDetailComponent },
+  { path: 'pokemon', loadChildren: () => import('./pokemon-list/pokemon.module').then(m => m.PokemonModule) },
   { path: '**', redirectTo: '/' },
 ];
 @NgModule({
   declarations: [
     AppComponent,
-    PokemonDetailComponent,
-    PokemonItemComponent,
-    PokemonListComponent,
     StockFormComponent,
     StockItemComponent,
     StockListComponent
@@ -32,7 +25,9 @@ const routes: Routes = [
     BrowserModule,
     ReactiveFormsModule,
     HttpClientModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes, {
+      preloadingStrategy: PreloadAllModules
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
