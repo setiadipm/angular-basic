3c0315ef64c66436cae66bcf8f4494f9913130cd
Setiadi Prawiryo Moeljadi <piyoo86@gmail.com>
2021-07-08 12:51:13 +0700

    Add kanban

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,29 @@
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 
+import { environment } from 'src/environments/environment.prod';
 import { AppComponent } from './app.component';
+import { AuthComponent } from './auth/auth.component';
+import { KanbanComponent } from './kanban/kanban.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 import { StockFormComponent } from './stock-form/stock-form.component';
 import { StockItemComponent } from './stock-item/stock-item.component';
 import { StockListComponent } from './stock-list/stock-list.component';
-import { AuthComponent } from './auth/auth.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
-import { environment } from 'src/environments/environment.prod';
 
 const routes: Routes = [
   { path: '', component: StockListComponent },
   { path: 'pokemon', loadChildren: () => import('./pokemon-list/pokemon.module').then(m => m.PokemonModule) },
   { path: 'auth', component: AuthComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'kanban', component: KanbanComponent },
   { path: '**', redirectTo: '/' },
 ];
 
@@ -27,6 +31,7 @@ const routes: Routes = [
   declarations: [
     AppComponent,
     AuthComponent,
+    KanbanComponent,
     LoginComponent,
     RegisterComponent,
     StockFormComponent,
@@ -34,14 +39,16 @@ const routes: Routes = [
     StockListComponent
   ],
   imports: [
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule,
     BrowserModule,
-    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    DragDropModule,
     HttpClientModule,
+    ReactiveFormsModule,
     RouterModule.forRoot(routes, {
       preloadingStrategy: PreloadAllModules
-    }),
-    AngularFireModule.initializeApp(environment.firebaseConfig),
-    AngularFirestoreModule
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
