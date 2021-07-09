68d43c75b321131f921c25ade9a8b9e4959cd727
Setiadi Prawiryo <setiadi@esb.co.id>
2021-07-09 15:42:28 +0700

    Refactor

--- src/app/pokemon/pokemon.module.ts
+++ src/app/pokemon/pokemon.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
+
 import { PokemonDetailComponent } from './pokemon-detail/pokemon-detail.component';
 import { PokemonItemComponent } from './pokemon-item/pokemon-item.component';
 import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
